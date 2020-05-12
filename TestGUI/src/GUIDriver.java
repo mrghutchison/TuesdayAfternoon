75e63353e096172ee199dd5a0148701df3efeffd
G Hutchison <grant.hutchison@tdsb.on.ca>
2020-05-12 15:29:01 -0400

    added a button

--- TestGUI/src/GUIDriver.java
+++ TestGUI/src/GUIDriver.java
@@ -1,10 +1,19 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GUIDriver extends Application {
 
 	@Override
 	public void start(Stage primary) throws Exception {
+		VBox vbox = new VBox();
+		Button btnHello = new Button("Hello");
+		btnHello.setMinSize(400, 200);
+		vbox.getChildren().add(btnHello);
+		Scene scene = new Scene(vbox, 400,200);
+		primary.setScene(scene);
 		primary.show();
 
 	}
